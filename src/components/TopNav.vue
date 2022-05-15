c1a9d05bb704974fdb5583a51a73982dfc8f81e3
ZivChen971206 <932483811@qq.com>
2022-05-15 22:53:19 +0800

    feat: 创建项目

---
+++ src/components/TopNav.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="topNav">
+        <div class="topLeft">
+            <i class="iconfont icon-liebiao"></i>
+        </div>
+        <div class="topCenter">
+            <span class="navBtn">我的</span>
+            <span class="navBtn">发现</span>
+            <span class="navBtn">云村</span>
+            <span class="navBtn">视频</span>
+        </div>
+        <div class="topRight">
+            <i class="iconfont icon-sousuo"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopNav',
+};
+</script>
+
+<style scoped lang="less">
+.topNav {
+    width: 7.5rem;
+    height: 0.8rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 0.2rem;
+    .topCenter {
+        width: 4.5rem;
+        display: flex;
+        justify-content: space-around;
+        .active{
+          font-weight: 900;
+        }
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <top-nav></top-nav>
+    </div>
+</template>
+
+<script>
+import TopNav from '@/components/TopNav.vue';
+
+export default {
+    name: 'Home',
+    components: {
+        TopNav,
+    },
+};
+</script>
+
+<style lang="less" scoped>
+#d1 {
+    width: 3.75rem;
+    height: 3.75rem;
+    background-color: skyblue;
+}
+</style>
